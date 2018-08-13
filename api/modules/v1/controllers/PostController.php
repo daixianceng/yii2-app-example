5ea81c823a59842fe8ef9dd06647cf22ebcb1da1
Cosmo <daixianceng@gmail.com>
2018-08-13 23:38:56 +0800

    feat(PostController): rewrite the `findModel` method

--- api/modules/v1/controllers/PostController.php
+++ api/modules/v1/controllers/PostController.php
@@ -2,6 +2,20 @@
 namespace api\modules\v1\controllers;
 
 use Yii;
+use yii\web\NotFoundHttpException;
+use common\models\Post;
 
 class PostController extends \api\common\controllers\PostController
-{ }
+{
+    /**
+     * @inheritdoc
+     */
+    protected function findModel($id)
+    {
+        if (($model = Post::findByKey($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
